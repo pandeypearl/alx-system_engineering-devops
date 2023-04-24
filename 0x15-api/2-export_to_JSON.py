8a46d3d0b63339f438f71601b0ddf2b604f99498
Pretty Pandey <prettypandeypearl@gmail.com>
2023-04-24 04:17:22 -0700

    Export to JSON

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,33 +1,35 @@
 #!/usr/bin/python3
 """ Script using JSONPlaceholder API to get info about employee
 and converts data to json """
+
+
 import json
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    user_id = sys.argv[1]
+    userId = sys.argv[1]
     user = requests.get(
             'https://jsonplaceholder.typicode.com/users/{}'
-            .format(user_id))
+            .format(userId))
 
     name = user.json().get('username')
     todos = requests.get('https://jsonplaceholder.typicode.com/todos')
 
-    todo_user = {}
-    task_list = []
+    todoUser = {}
+    taskList = []
 
     for task in todos.json():
-        if task.get('user_id') == int(user_id):
-            task_dict = {
+        if task.get('userId') == int(userId):
+            taskDict = {
                         "task": task.get('title'),
                         "completed": task.get('completed'),
                         "username": user.json().get('username')
                         }
-            task_list.append(task_dict)
-        todo_user[user_id] = task_list
+            taskList.append(taskDict)
+        todoUser[userId] = taskList
 
-    filename = user_id + '.json'
-    with open(filename, mode='w') as f:
-        json.dump(todo_user, f)
+    filename = userId + '.json'
+    with open(filename, mode="w") as f:
+        json.dump(todoUser, f)
