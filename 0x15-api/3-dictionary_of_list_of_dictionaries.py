725be2807269c0d941b752167402ca6d6d5e45b3
Pretty Pandey <prettypandeypearl@gmail.com>
2023-04-24 04:45:36 -0700

    Dictionary of list of dictionaries

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Script using JSONPlaceholder API to get info about employee
+and converts data to json """
+
+
+if __name__ == "__main__":
+
+    import json
+    import requests
+    import sys
+
+    users = requests.get('https://jsonplaceholder.typicode.com/users')
+    users = users.json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todos = todos.json()
+    todoAll = {}
+
+    for user in users:
+        taskList = []
+        for task in todos:
+            if task.get('userId') == user.get('id'):
+                taskDict = {
+                        "username": user.get('username'),
+                        "task": task.get('title'),
+                        "completed": task.get('completed')
+                        }
+                taskList.append(taskDict)
+        todoAll[user.get('id')] = taskList
+
+    with open('todo_all_employees.json', mode="w") as f:
+        json.dump(todoAll, f)
